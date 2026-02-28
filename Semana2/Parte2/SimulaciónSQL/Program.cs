f944c7365a0402c945af7d18f3400fd0e6e65b55
kacunacollado-netizen <kacunacollado@gmail.com>
2026-02-27 22:46:07 -0600

    Add files via upload

---
+++ Semana2/Parte2/SimulaciónSQL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string consulta =
+            "SELECT * FROM Usuarios WHERE usuario = @usuario AND password = @password";
+
+        Console.WriteLine("Consulta SQL segura:");
+        Console.WriteLine(consulta);
+
+        // Se usan parámetros para evitar inyección SQL porque es más seguro que ocntatenar, ya que al contatenar se mezcla información SQL( código) con datos que puede ingresar el usuario.
+
+        Console.ReadLine();
+    }
+}
