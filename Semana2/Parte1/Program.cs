036791d32a4a81891e7db807615c4f71c4679f0b
kacunacollado-netizen <kacunacollado@gmail.com>
2026-02-27 22:32:09 -0600

    Add files via upload

---
+++ Semana2/Parte1/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> estudiantes = new List<string>()
+{
+    "Luis",
+    "Ana",
+    "Carlos",
+    "María"
+};
+
+foreach (string nombre in estudiantes)
+{
+    if (nombre == "Ana")
+    {
+        continue;
+    }
+
+    Console.WriteLine("Hola " + nombre);
+}
