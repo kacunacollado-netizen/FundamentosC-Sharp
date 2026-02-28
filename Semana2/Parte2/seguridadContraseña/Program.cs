dd47787f42b56a9efdbbf4610daa6cf28fdaa400
kacunacollado-netizen <kacunacollado@gmail.com>
2026-02-27 22:45:31 -0600

    Add files via upload

---
+++ Semana2/Parte2/seguridadContraseña/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+string password = "Pokemon4nivers4rio";
+string salt = Guid.NewGuid().ToString();
+
+string passwordConSalt = password + salt;
+
+SHA256 sha256 = SHA256.Create();
+byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(passwordConSalt));
+
+string hash = Convert.ToBase64String(bytes);
+
+Console.WriteLine("Contraseña original: " + password);
+Console.WriteLine("Salt: " + salt);
+Console.WriteLine("Hash generado: " + hash);
