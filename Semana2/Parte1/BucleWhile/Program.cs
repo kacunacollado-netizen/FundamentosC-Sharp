60a8afcee56685ebdc8d68d1444a842bd0b64e36
kacunacollado-netizen <kacunacollado@gmail.com>
2026-02-27 22:48:31 -0600

    Add files via upload

---
+++ Semana2/Parte1/BucleWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿int numero;
+
+while (true)
+{
+    Console.Write("Ingrese un número positivo o digite 0 para salir: ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero == 0)
+    {
+        break;
+    }
+
+    if (numero > 100)
+    {
+        Console.WriteLine("Número mayor a 100. Fin del programa.");
+        break;
+    }
+
+    Console.WriteLine("Número ingresado: " + numero);
+}
