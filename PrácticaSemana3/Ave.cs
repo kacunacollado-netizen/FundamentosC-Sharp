b9f15001b9ad406cad724496727de2593397b9ef
kacunacollado-netizen <kacunacollado@gmail.com>
2026-03-06 22:06:41 -0600

    Add files via upload

---
+++ PrácticaSemana3/Ave.cs
@@ -0,0 +1,17 @@
+﻿using AnimalS3;
+using System;
+
+namespace AveS3
+{
+    public class Ave : Animal
+    {
+        public Ave(string nombre, int edad) : base(nombre, edad)
+        {
+        }
+
+        public override void EmitirSonido()
+        {
+            Console.WriteLine("El ave " + GetNombre() + " hace: Pio Pio");
+        }
+    }
+}
