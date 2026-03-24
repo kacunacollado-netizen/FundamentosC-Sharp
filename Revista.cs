d2320f610c23c6974f7a89664326eeac9cc68574
kacunacollado-netizen <kacunacollado@gmail.com>
2026-03-23 22:11:23 -0600

    Add files via upload

---
+++ Revista.cs
@@ -0,0 +1,24 @@
+﻿// La clase Revista también hereda de Producto
+
+public class Revista : Producto
+{
+    // Atributo propio de las revistas
+    public string Editorial { get; set; }
+
+    public Revista(string id, string nombre, string editorial)
+        : base(id, nombre)
+    {
+        Editorial = editorial;
+    }
+
+    // Polimorfismo: sobrescribimos el método MostrarInfo
+    public override void MostrarInfo()
+    {
+        Console.WriteLine($"Revista -> ID: {Id} | Nombre: {Nombre} | Editorial: {Editorial}");
+    }
+
+    public override void CalcularPrecio()
+    {
+        // Se agregará en el segundo avance
+    }
+}
